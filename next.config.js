670143cf8235808a0270afbf062e32df936499b9
atsushi-yamakawa-design <96972564+atsushi-yamakawa-design@users.noreply.github.com>
2023-06-12 02:33:19 +0900

    いろんな設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+const path = require('path');
+
+module.exports = {
+  webpack: (config) => {
+    // パスのエイリアスを追加する場合はここ
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.resolve.alias['@styles'] = path.join(__dirname, 'src', 'styles');
+    return config;
+  },
+};
