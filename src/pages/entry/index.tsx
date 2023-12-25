7cb14265f59fc1af075a11aba88373bcb50338aa
atsushi-yamakawa-design <96972564+atsushi-yamakawa-design@users.noreply.github.com>
2023-12-26 01:48:24 +0900

    エントリーページ・hover調整

---
+++ src/pages/entry/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import style from './entry.module.scss';
+import { client } from '@libs/client';
+
+export default function EntryPage({}: {}) {
+  return (
+    <>
+      <div>エントリーページ</div>
+    </>
+  );
+}
