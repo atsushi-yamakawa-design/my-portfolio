bd2db72175612688a0ca83d73bbf1c515f17773e
atsushi-yamakawa-design <96972564+atsushi-yamakawa-design@users.noreply.github.com>
2024-01-16 02:22:37 +0900

    scss変数に変換するロジック

--- convertTokens.js
+++ convertTokens.js
@@ -1,19 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 
-const designTokens = require('./src/styles/designTokens.json');
+// JSONファイルのパスを指定
+const jsonFilePath = path.join(__dirname, './src/styles/designTokens.json');
 
+// SCSSファイルの出力先を指定
+const scssFilePath = path.join(__dirname, './src/styles/tokenVariables.scss');
+
+const designTokens = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+// SCSS変数に変換する関数
 function convertTokensToScss(tokens) {
   let scss = '';
-  for (const key in tokens) {
-    const token = tokens[key];
-    scss += `$${key}: ${token.value};\n`;
+
+  // 'global' キーの中のトークンを処理
+  for (const [key, token] of Object.entries(tokens.global)) {
+    // フォントファミリーの値にシングルクォートを追加
+    let value =
+      token.type === 'fontFamilies' ? `'${token.value}'` : token.value;
+
+    // 参照をSCSS変数に変換（例："{d-1}" を "$d-1" に変換）
+    value = value.replace(/{(.*?)}/g, (_, match) => `$${match}`);
+
+    scss += `$${key}: ${value};\n`;
   }
   return scss;
 }
 
+// 変換実行
 const scssContent = convertTokensToScss(designTokens);
-fs.writeFileSync(
-  path.join(__dirname, 'src/styles/tokenVariables.scss'),
-  scssContent
-);
+
+// 結果をファイルに書き出し
+fs.writeFileSync(scssFilePath, scssContent);
+
+console.log(`SCSS file has been generated at ${scssFilePath}`);
